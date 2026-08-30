23f50828dbecd6d876e468d900dd5408843dbb92
“Chris-Skogstad” <21198609@students.latrobe.edu.au>
2026-08-30 09:19:50 +0000

    code commit

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['44.210.109.160', 'localhost', '127.0.0.1'], // change to your IP in production
 };
 
 export default nextConfig;
